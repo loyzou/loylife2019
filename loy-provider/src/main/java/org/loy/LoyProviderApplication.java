86eba3fa77725a4b076a28f39f48c99462bb5352
loyzou <zouloy@163.com>
2019-11-13 10:40:13 +0800

    add provider

--- loy-provider/src/main/java/org/loy/LoyProviderApplication.java
+++ loy-provider/src/main/java/org/loy/LoyProviderApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"org.loy.*"})
 @Slf4j
 public class LoyProviderApplication {
 
