b346fc5272e5856e597daa8461903492744ba7a0
loyzou <zouloy@163.com>
2020-03-17 17:19:07 +0800

    add config

--- loy-eureka/src/main/java/org/loy/LoyEurekaApplication.java
+++ loy-eureka/src/main/java/org/loy/LoyEurekaApplication.java
@@ -1,13 +1,11 @@
 package org.loy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.loy.common.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaServer
 @Slf4j
 public class LoyEurekaApplication {
