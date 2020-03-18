b5c05dae5cbd10ea34328cfaef3d47021f18d7f5
loyzou <zouloy@163.com>
2020-03-18 17:04:01 +0800

    add redis

--- loy-consumer/src/main/java/org/loy/controller/order/OrderController.java
+++ loy-consumer/src/main/java/org/loy/controller/order/OrderController.java
@@ -5,15 +5,19 @@ import org.loy.common.context.LoyParam;
 import org.loy.common.context.LoyResult;
 import org.loy.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
 @RequestMapping("/account/order/")
 public class OrderController extends BaseController {
     @Autowired
     private ILoyApi iLoyApi;
+    @Autowired
+    RedisTemplate redisTemplate;
 
     /***
      * 查询订单列表
@@ -24,5 +28,16 @@ public class OrderController extends BaseController {
         LoyParam loyParam = new LoyParam("orderService", "queryOrderList");
         LoyResult result = iLoyApi.callNoTx(loyParam);
         return result;
+    }
+
+    @RequestMapping("queryByRedis")
+    public LoyResult queryByRedis(){
+        LoyResult loyResult = new LoyResult();
+        String loy = (String)redisTemplate.opsForValue().get("loy");
+        if(StringUtils.isEmpty(loy)){
+            redisTemplate.opsForValue().set("loy","loy2");
+        }
+        loyResult.setMessage(loy);
+        return loyResult;
     }
 }
