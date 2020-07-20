e3877a93c33c8824ad96c27a60f0ce20002cfffb
邹全洪 <zouquanhong@rainbowcn.com>
2020-07-20 16:25:43 +0800

    closeable test

---
+++ demo/src/main/java/org/loy/demo_closeable/CloseAbleDemo.java
@@ -0,0 +1,27 @@
+/*
+ * 深圳市灵智数科有限公司版权所有.
+ */
+package org.loy.demo_closeable;
+
+/**
+ * 实现closeAblede接口的类，可以不用手动在finally进行资源的关闭，执行完try
+ * 之后，代码会自动执行close
+ *
+ * @author 邹全洪
+ * @version 1.0.0
+ * @date 2020/7/17
+ */
+public class CloseAbleDemo implements AutoCloseable {
+    @Override
+    public void close() {
+        System.out.println("自动调用close关闭流");
+    }
+
+    public static void main(String[] args) {
+        try(CloseAbleDemo closeAbleDemo = new CloseAbleDemo()) {
+        } catch (Exception e) {
+            System.out.println("--exception--");
+        }
+
+    }
+}
