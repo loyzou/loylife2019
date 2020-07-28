28c542c9b076c9a90147318b30d74a8b7f9de0c6
邹全洪 <zouquanhong@rainbowcn.com>
2020-07-28 17:11:57 +0800

    新增中断操作demo

---
+++ loy-demo/src/main/java/org/loy/thread_demo/InterruptedDemo1.java
@@ -0,0 +1,38 @@
+/*
+ * 深圳市灵智数科有限公司版权所有.
+ */
+package org.loy.thread_demo;
+
+/**
+ * 中断demo
+ *
+ * @author 邹全洪
+ * @version 1.0.0
+ * @date 2020/7/28
+ */
+public class InterruptedDemo1 extends Thread {
+    private static boolean isInterrupted = false;
+
+    @Override
+    public void run() {
+
+        while (!isInterrupted) {
+            System.out.println("线程执行中。。。。。。");
+            Thread.yield();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        InterruptedDemo1 interruptedDemo1 = new InterruptedDemo1();
+        interruptedDemo1.start();
+
+        // 中断线程
+        try {
+            Thread.sleep(5000);
+            isInterrupted = true;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ loy-demo/src/main/java/org/loy/thread_demo/InterruptedDemo2.java
@@ -0,0 +1,36 @@
+/*
+ * 深圳市灵智数科有限公司版权所有.
+ */
+package org.loy.thread_demo;
+
+/**
+ * 通过监听是否中断的方式，进行中断
+ *
+ * @author 邹全洪
+ * @version 1.0.0
+ * @date 2020/7/28
+ */
+public class InterruptedDemo2 extends Thread {
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.println("进行正常操作");
+        }
+
+        System.out.println("。。。。。中断操作完成。。。");
+
+    }
+
+    public static void main(String[] args) {
+        InterruptedDemo2 interruptedDemo2 = new InterruptedDemo2();
+        interruptedDemo2.start();
+
+        try {
+            Thread.sleep(10000);
+            System.out.println("进行中断中。。。。");
+            interruptedDemo2.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
